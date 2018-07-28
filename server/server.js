296f74b1b24096b2f2b408b3db79c15a94caaa37
MIraK1E <mike.pattana@gmail.com>
2018-07-28 18:48:56 +0700

    Greet new  user and alert others

--- server/server.js
+++ server/server.js
@@ -16,6 +16,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User Connected');
 
+    socket.emit('newMessage', { message: 'Welcome to the chat app' });
+
+    // send to all connected user except for the one who join
+    socket.broadcast.emit('newMessage', { 
+        from: 'admin', 
+        text: 'New User Join',
+        createdAt: new Date().getTime() 
+    });
+
     socket.on('createMessage', (message) => {
         console.log('created message', message);
         io.emit('newMessage', {
@@ -23,6 +32,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.form,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
